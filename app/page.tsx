875f1bbd3ee955ed9a0b5f660f92c219cc5fa541
Emmanuel Ochieng <eochieng16@gmail.com>
2026-05-15 14:21:50 +0300

    page -> setup for useLogoScrollAnimation

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
+"use client";
+
+import { useRef } from "react";
+import { useLogoScrollAnimation } from "@/hooks/useLogoScrollAnimation";
+
 import Image from "next/image";
 
 export default function Home() {
+
+  const heroLogoRef = useRef<HTMLHeadingElement>(null);
+  const navbarRef = useRef<HTMLElement>(null);
+  const navLogoRef = useRef<HTMLSpanElement>(null);
+
+  useLogoScrollAnimation({ heroLogoRef, navbarRef, navLogoRef });
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans ">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white sm:items-start">
